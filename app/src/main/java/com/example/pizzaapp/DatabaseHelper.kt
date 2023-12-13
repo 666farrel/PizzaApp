7add6dcfc7ec0936308db6c99d2a4af0e27b6d9e
666farrel <farrelfst@students.amikom.ac.id>
2023-12-13 16:53:54 +0700

    update

--- app/src/main/java/com/example/pizzaapp/DatabaseHelper.kt
+++ app/src/main/java/com/example/pizzaapp/DatabaseHelper.kt
@@ -1,5 +1,6 @@
 package com.example.pizzaapp
 
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Context
 import android.database.Cursor
@@ -14,13 +15,16 @@ import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 
 class DatabaseHelper(var context:Context):SQLiteOpenHelper(
-    context,DATABASE_NAME, null,DATABASE_VERSION )  {
+    context,DATABASE_NAME, null,DATABASE_VERSION ) {
+
 
     companion object {
         private val DATABASE_NAME = "pizza"
         private val DATABASE_VERSION = 1
+
         //table name
         private val TABLE_ACCOUNT = "account"
+
         //column account table
         private val COLUMN_EMAIL = "email"
         private val COLUMN_NAME = "name"
@@ -28,73 +32,114 @@ class DatabaseHelper(var context:Context):SQLiteOpenHelper(
         private val COLUMN_PASSWORD = "password"
 
         //table menu
-        private val  TABLE_MENU = "Menu"
+        private val TABLE_MENU = "Menu"
+
         //column table
         private val COLUMN_ID_MENU = "idMenu"
         private val COLUMN_NAMA_MENU = "menuName"
         private val COLUMN_PRICE_MENU = "price"
         private val COLUMN_IMAGE = "photo"
 
+        //table transaksi
+        private val TABLE_TRANS = "transaksi"
+
+        //column tabel transaksi
+        private val COLUMN_ID_TRANS = "idTransaksi"
+        private val COLUMN_TGL = "tanggal"
+        private val COLUMN_USER = "user"
+
+        //table detail transaksi
+        private val TABLE_DET_TRANSACTION = "detailTrans"
+
+        //column table detail trans
+        private val COLUMN_ID_DET_TRX = "idDetailTrx"
+        private val COLUMN_ID_TRX = "idTransaksi"
+        private val COLUMN_ID_PESAN = "idMenu"
+        private val COLUMN_HARGA_PESAN = "harga"
+        private val COLUMN_JUMLAH = "jumlah"
+
     }
+
     //create table account sql query
     private val CREATE_ACCOUNT_TABLE = ("CREATE TABLE" + TABLE_ACCOUNT + "("
-            + COLUMN_EMAIL + " TEXT PRIMARY KEY, "+COLUMN_NAME +" TEXT, "
-            + COLUMN_LEVEL + "TEXT, "+ COLUMN_PASSWORD +" TEXT)")
+            + COLUMN_EMAIL + " TEXT PRIMARY KEY, " + COLUMN_NAME + " TEXT, "
+            + COLUMN_LEVEL + "TEXT, " + COLUMN_PASSWORD + " TEXT)")
 
     //drop table account sql query
     private val DROP_ACCOUNT_TABLE = "DROP TABLE IF EXISTS $TABLE_ACCOUNT"
 
     // create table menu sql query
     private val CREATE_MENU_TABLE = ("CREATE " + TABLE_MENU + "("
-            + COLUMN_ID_MENU + " INT PRIMARY KEY, "+ COLUMN_NAMA_MENU +" TEXT, "
-            + COLUMN_PRICE_MENU + " INT, "+ COLUMN_IMAGE +" BLOB)")
+            + COLUMN_ID_MENU + " INT PRIMARY KEY, " + COLUMN_NAMA_MENU + " TEXT, "
+            + COLUMN_PRICE_MENU + " INT, " + COLUMN_IMAGE + " BLOB)")
 
     // drop table menu sql query
     private val DROP_MENU_TABLE = "DROP TABLE IF EXISTS $TABLE_MENU"
 
+    //create table transaksi sql query
+    private val CREATE_TRANSACTION_TABLE = ("CREATE TABLE " + TABLE_TRANS + "("
+            + COLUMN_ID_TRANS + " INT PRIMARY KEY, " + COLUMN_TGL + " TEXT, "
+            + COLUMN_USER + " TEXT)")
+
+    //drop table transaksi sql query
+    private val DROP_TRANSACTION_TABLE = "DROP TABLE IF EXISTS $TABLE_TRANS"
+
+    //create table detail transaksi sql query
+    private val CREATE_DET_TRANS_TABLE = ("CREATE TABLE " + TABLE_DET_TRANSACTION + "("
+            + COLUMN_ID_DET_TRX + " INT PRIMARY KEY, " + COLUMN_ID_TRX + " INT, "
+            + COLUMN_ID_PESAN + " INT, " + COLUMN_HARGA_PESAN + " INT, "
+            + COLUMN_JUMLAH + " INT)")
+
+    //drop table detail transaksi sql query
+    private val DROP_DET_TRANS_TABLE = "DROP TABLE IF EXISTS $TABLE_DET_TRANSACTION"
+
     override fun onCreate(p0: SQLiteDatabase?) {
-        TODO("Not yet implemented")
         p0?.execSQL(CREATE_ACCOUNT_TABLE)
         p0?.execSQL(CREATE_MENU_TABLE)
+        p0?.execSQL(CREATE_TRANSACTION_TABLE)
+        p0?.execSQL(CREATE_DET_TRANS_TABLE)
         p0?.execSQL(INSERT_ACCOUNT_TABLE)
     }
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
-        TODO("Not yet implemented")
         p0?.execSQL(DROP_ACCOUNT_TABLE)
         p0?.execSQL(DROP_MENU_TABLE)
+        p0?.execSQL(DROP_TRANSACTION_TABLE)
+        p0?.execSQL(DROP_DET_TRANS_TABLE)
         onCreate(p0)
     }
 
     //login check
-    fun checkLogin(email:String, password:String):Boolean {
+    fun checkLogin(email: String, password: String): Boolean {
         val colums = arrayOf(COLUMN_NAME)
         val db = this.readableDatabase
         //selection criteria
         val selection = "$COLUMN_EMAIL = ? AND $COLUMN_PASSWORD = ?"
         //selection arguments
-        val selectionArgs = arrayOf(email,password)
+        val selectionArgs = arrayOf(email, password)
 
-        val cursor = db.query(TABLE_ACCOUNT,
-        colums,
-        selection,
-        selectionArgs,
-        null,
+        val cursor = db.query(
+            TABLE_ACCOUNT,
+            colums,
+            selection,
+            selectionArgs,
+            null,
             null,
             null,
         )
-        val cursorCount =cursor.count
+        val cursorCount = cursor.count
         cursor.close()
         db.close()
 
         //check data available or not
-        if(cursorCount > 0)
+        if (cursorCount > 0)
             return true
         else
             return false
     }
+
     //add user
-    fun addAccount(email:String, name:String, level:String, password:String){
+    fun addAccount(email: String, name: String, level: String, password: String) {
         val db = this.readableDatabase
 
         val values = ContentValues()
@@ -107,27 +152,32 @@ class DatabaseHelper(var context:Context):SQLiteOpenHelper(
         //show massage
         if (result == (0).toLong()) {
             Toast.makeText(context, "Register Failed", Toast.LENGTH_SHORT).show()
-        }
-        else {
-            Toast.makeText(context, "Register Success, " +
-                "Please Login UsingYour New Account", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(
+                context, "Register Success, " +
+                        "Please Login UsingYour New Account", Toast.LENGTH_SHORT
+            ).show()
         }
         db.close()
     }
-    fun  checkData (email: String):String{
+
+    @SuppressLint("Range")
+    fun checkData(email: String): String {
         val colums = arrayOf(COLUMN_NAME)
         val db = this.readableDatabase
         val selection = "$COLUMN_EMAIL = ?"
         val selectionArgs = arrayOf(email)
-        var name:String = ""
+        var name: String = ""
 
-        val cursor = db.query(TABLE_ACCOUNT,
-        colums,
-        selection,
-        selectionArgs,
-        null,
-        null,
-        null)
+        val cursor = db.query(
+            TABLE_ACCOUNT,
+            colums,
+            selection,
+            selectionArgs,
+            null,
+            null,
+            null
+        )
 
         if (cursor.moveToFirst()) {
             name = cursor.getString(cursor.getColumnIndex(COLUMN_NAME))
@@ -137,81 +187,48 @@ class DatabaseHelper(var context:Context):SQLiteOpenHelper(
         return name
     }
 
-        //add new menu
-        fun addMenu(menu:MenuModel) {
-            val db = this.writableDatabase
-            val values = ContentValues()
-            values.put(COLUMN_ID_MENU, menu.id)
-            values.put(COLUMN_NAMA_MENU, menu.name)
-            values.put(COLUMN_PRICE_MENU, menu.price)
-            //prepare image
-            val byteOutputStream = ByteArrayOutputStream()
-            val imageInByte:ByteArray
-            menu.image.compress(Bitmap.CompressFormat.JPEG, 100, byteOutputStream)
-            imageInByte = byteOutputStream.toByteArray()
-            values.put(COLUMN_IMAGE, imageInByte)
+    //add new menu
+    fun addMenu(menu: MenuModel) {
+        val db = this.writableDatabase
+        val values = ContentValues()
+        values.put(COLUMN_ID_MENU, menu.id)
+        values.put(COLUMN_NAMA_MENU, menu.name)
+        values.put(COLUMN_PRICE_MENU, menu.price)
+        //prepare image
+        val byteOutputStream = ByteArrayOutputStream()
+        val imageInByte: ByteArray
+        menu.image.compress(Bitmap.CompressFormat.JPEG, 100, byteOutputStream)
+        imageInByte = byteOutputStream.toByteArray()
+        values.put(COLUMN_IMAGE, imageInByte)
 
-            val result = db.insert(TABLE_MENU, null, values)
-            //show massage
-            if (result == (0).toLong()) {
-                Toast.makeText(context, "Add Menu Failed", Toast.LENGTH_SHORT).show()
-            }
-            else {
-                Toast.makeText(context, "Add Menu Success", Toast.LENGTH_SHORT).show()
-            }
-            db.close()
+        val result = db.insert(TABLE_MENU, null, values)
+        //show massage
+        if (result == (0).toLong()) {
+            Toast.makeText(context, "Add Menu Failed", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(context, "Add Menu Success", Toast.LENGTH_SHORT).show()
         }
-    fun showMenu():ArrayList<MenuModel> {
-        val listModel = ArrayList<MenuModel> ()
-        val db = this.readableDatabase
-        val cursor: Cursor?=null
-        try {
-            cursor = db.rawQuery("SELECT * FORM" + TABLE_MENU, null)
-        } catch (se::SQLiteException) {
-            db.execSQL(CREATE_MENU_TABLE)
-            return ArrayList()
-        }
-
-        var id:Int
-        var name:String
-        var price:Int
-        var imageArray:ByteArray
-        var imageBmp:Bitmap
-
-        if (cursor.moveToFirst()) {
-            do {
-                //get data text
-                id = cursor.getInt(cursor.getColumnIndex(COLUMN_ID_MENU))
-                name = cursor.getString(cursor.getColumnIndex(COLUMN_NAMA_MENU))
-                price = cursor.getInt(cursor.getColumnIndex(COLUMN_PRICE_MENU))
-                //get data image
-                imageArray = cursor.getBlob(cursor.getColumnIndex(COLUMN_IMAGE))
-                //convert ByteArray to Bitmap
-                val byteInputStream = ByteArrayInputStream(imageArray)
-                imageBmp = BitmapFactory.decodeStream(byteInputStream)
-                val model = MenuModel(id = id, name =  name, price = price, image = imageBmp)
-                listModel.add(model)
-            } while (cursor.moveToNext())
-        }
-        return listModel
+        db.close()
     }
 
-    fun showMenu() : ArrayList<MenuModel> {
+
+    @SuppressLint("Range")
+    fun showMenu(): ArrayList<MenuModel> {
         val listModel = ArrayList<MenuModel>()
         val db = this.readableDatabase
-        var  cursor:Cursor?=null
+        var cursor: Cursor? = null
         try {
             cursor = db.rawQuery("SELECT * FROM" + TABLE_MENU, null)
-        } catch (se:SQLException) {
+        } catch (se: SQLException) {
             db.execSQL(CREATE_MENU_TABLE)
             return ArrayList()
         }
 
-        var id:Int
-        var name:String
-        var price:Int
-        var imageArray:ByteArray
-        var imageBmp:Bitmap
+        var id: Int
+        var name: String
+        var price: Int
+        var imageArray: ByteArray
+        var imageBmp: Bitmap
 
         if (cursor.moveToFirst()) do {
             //get data text
@@ -223,9 +240,78 @@ class DatabaseHelper(var context:Context):SQLiteOpenHelper(
             //convert ByteArray to Bitmap
             val byteInputStream = ByteArrayInputStream(imageArray)
             imageBmp = BitmapFactory.decodeStream(byteInputStream)
-            val model = MenuModel(id =id, name = name, price = price, image = imageBmp )
+            val model = MenuModel(id = id, name = name, price = price, image = imageBmp)
             listModel.add(model)
         } while (cursor.moveToNext())
-    return  listModel
+        return listModel
+    }
+
+    //add new transaksi
+    @SuppressLint("Range")
+    fun addTransaction() {
+        val dbInsert = this.writableDatabase
+        val dbSelect = this.readableDatabase
+        //declare var
+        var lastIdTrans = 0
+        var lastIdDetail = 0
+        var newIdTrans = 0
+        var newIdDetail = 0
+        val values = ContentValues()
+        //get last idTransaksi
+        val cursorTrans: Cursor = dbSelect.rawQuery(
+            "SELECT  * FROM $TABLE_TRANS", null
+        )
+
+        val cursorDetail: Cursor = dbSelect.rawQuery(
+            "SELECT *  FROM $TABLE_DET_TRANSACTION", null
+        )
+
+        if (cursorTrans.moveToLast()) {
+            lastIdTrans = cursorTrans.getInt(0) //to get id, 0 is the column index
+        }
+
+        if (cursorDetail.moveToLast()) {
+            lastIdDetail = cursorDetail.getInt(0) //to get id, 0 is the column index
+        }
+        //set data
+        newIdTrans = lastIdTrans + 1
+        val sdf = SimpleDateFormat("yyyy-MM-dd")
+        val tanggal = sdf.format(Date())
+        val username = FragmentProfile.email
+        //insert data transaksi
+        values.put(COLUMN_ID_TRANS, newIdTrans)
+        values.put(COLUMN_TGL, tanggal)
+        values.put(COLUMN_USER, username)
+        val result = dbInsert.insert(TABLE_TRANS,null, values)
+        //show message
+        if (result==(0).toLong()){
+            Toast.makeText(context, "Add transaction Failed", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(context, "Add transaction Success",Toast.LENGTH_SHORT).show()
+        }
+
+        newIdDetail = lastIdDetail + 1
+        var i = 0
+        val values2 = ContentValues()
+        while(i < TransaksiAdapter.listId.count()){
+            values2.put(COLUMN_ID_DET_TRX, newIdDetail)
+            values2.put(COLUMN_ID_TRX, newIdTrans)
+            values2.put(COLUMN_ID_PESAN, TransaksiAdapter.listId[i])
+            values2.put(COLUMN_HARGA_PESAN, TransaksiAdapter.listHarga[i])
+            values2.put(COLUMN_JUMLAH, TransaksiAdapter.listJumlah[i])
+            val result2 = dbInsert.insert(TABLE_DET_TRANSACTION,null, values2)
+            //show message
+            if (result2==(0).toLong()){
+                Toast.makeText(context, "Add detail Failed", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(context, "Add detail Success",Toast.LENGTH_SHORT).show()
+            }
+            newIdDetail += 1
+            i+=1
+        }
+        dbSelect.close()
+        dbInsert.close()
+        }
     }
 }
